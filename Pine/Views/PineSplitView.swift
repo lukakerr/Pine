d33a59385c014813334ba5dc7e5139ca0b0342ff
lukakerr <lukakerr1@gmail.com>
2019-03-22 15:03:20 +1100

    Update split view orientation when preference changes

--- Pine/Views/PineSplitView.swift
+++ Pine/Views/PineSplitView.swift
@@ -1,5 +1,5 @@
 //
-//  EditorSplitView.swift
+//  PineSplitView.swift
 //  Pine
 //
 //  Created by Luka Kerr on 14/6/18.
@@ -8,9 +8,17 @@
 
 import Cocoa
 
-class EditorSplitView: NSSplitView {
+class PineSplitView: NSSplitView {
+
+  override func viewDidMoveToWindow() {
+    NotificationCenter.receive(.preferencesChanged, instance: self, selector: #selector(updateUI))
+  }
 
   override func viewWillDraw() {
+    self.updateUI()
+  }
+
+  @objc private func updateUI() {
     // when isVertical is true, split views drawn as V | V
     isVertical = preferences[Preference.verticalSplitView]
   }
